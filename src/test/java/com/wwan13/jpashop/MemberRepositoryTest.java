178defaa091d5e4bc9462a9fe0f32a58233a5c25
wwan13 <wwan13@naver.com>
2023-05-10 00:03:25 +0900

    [feat]: init projact, db connection

---
+++ src/test/java/com/wwan13/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.wwan13.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional // Transactional 애노테이션이 test 에 적용돼 있으면 테스트가 끝나고 자동으로 rollback 하기 떄
+    @Rollback(value = false) //rollback 애노테이션을 추가로 적용해준다.
+    public void testMember() {
+
+        Member member = new Member();
+        member.setUsername("memberA");
+        Long savedId = memberRepository.save(member);
+
+        Member findMember = memberRepository.find(savedId);
+
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+    }
+
+}
