0955830369561e846d7b7e91cf9646e7cb37b341
wwan13 <wwan13@naver.com>
2023-05-16 22:33:40 +0900

    [feat]: item order orderitem business logic

--- src/main/java/com/wwan13/jpashop/domain/Item.java
+++ src/main/java/com/wwan13/jpashop/domain/Item.java
@@ -1,5 +1,6 @@
 package com.wwan13.jpashop.domain;
 
+import com.wwan13.jpashop.exeption.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,5 +26,17 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<Category>();
+
+    //==비즈니스 로직==//
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 
 }
