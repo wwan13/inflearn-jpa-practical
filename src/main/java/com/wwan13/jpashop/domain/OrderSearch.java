388b632f7d8db22f6f066bccd9de491e6d772815
wwan13 <wwan13@naver.com>
2023-05-17 12:26:36 +0900

    [faet]: search logic

---
+++ src/main/java/com/wwan13/jpashop/domain/OrderSearch.java
@@ -0,0 +1,11 @@
+package com.wwan13.jpashop.domain;
+
+import lombok.Getter;
+
+@Getter
+public class OrderSearch {
+
+    private String memberName; //회원 이름
+    private OrderStatus orderStatus;//주문 상태[ORDER, CANCEL]
+
+}
